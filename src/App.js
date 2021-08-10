c3c208a03d466c3f0141511106921bf0700b1312
gwillen <gabewillen@gmail.com>
2021-08-10 06:58:06 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
-import './App.css';
+import {Box, Grommet} from "grommet";
+import Web3Modal from "web3modal"
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import grommetTheme from "./themes/theme.json";
+import Web3 from "web3";
+import Trade from "./components/NativeSurgeTrader"
+
+const providerOptions = {
+    walletconnect: {
+        package: WalletConnectProvider, // required
+            options: {
+            infuraId: "INFURA_ID" // required
+        }
+    }
+};
+
+const web3Modal = new Web3Modal({
+    network: "mainnet", // optional
+    cacheProvider: true, // optional
+    providerOptions // required
+});
+
+
+
+// const provider = web3Modal.connect();
+
+// const web3 = new Web3(provider);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Grommet theme={grommetTheme}>
+          <Box align="center" pad="large" border={{ size: 'medium' }}>
+              <Trade/>
+          </Box>
+      </Grommet>
+
   );
 }
 
