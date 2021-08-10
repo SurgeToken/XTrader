69c0a01fdb357c3f326c2e49a1d693605de69833
gwillen <gabewillen@gmail.com>
2021-08-10 06:58:06 -0500

    initial

---
+++ src/components/NativeSurgeTrader.js
@@ -0,0 +1,66 @@
+import {Box, Form, FormField, Button, TextInput, Heading, Select, Tab, Tabs} from "grommet";
+import React from "react";
+import Web3 from "web3";
+
+const BuyForm = () => {
+    return (
+
+        <Form>
+            <Box align={"center"} pad={"medium"}>
+                <FormField
+                    label="Surge"
+                    name="xToken"
+                    component={Select}
+                    onChange={(event) => console.log(event)}
+                    options={['sBNB', 'sUSD', 'sBTC', 'sETH']}
+                />
+                <TextInput
+                    label="To"
+                    placeholder={"amount"}
+                />
+            </Box>
+        </Form>
+    )
+}
+
+const SellForm = () => {
+    const [value, setValue] = React.useState({});
+    return (
+
+        <Form>
+            <Box align={"center"} pad={"medium"}>
+                <FormField
+                    label="Surge"
+                    component={Select}
+                    onChange={(event) => console.log(event)}
+                    options={['sBNB', 'sUSD', 'sBTC', 'sETH']}
+                />
+
+                <TextInput
+                    label="To"
+                    placeholder={"quantity"}
+                />
+            </Box>
+        </Form>
+    )
+}
+
+
+export default () => {
+    return (
+        <Box border small round align={"center"} pad={"medium"} background={"dark-1"} >
+            <Tabs>
+                <Tab title={"Buy"}>
+                    <BuyForm/>
+                </Tab>
+                <Tab title={"Sell"}>
+                    <SellForm/>
+                </Tab>
+            </Tabs>
+            <Box direction="row" gap="medium" >
+                <Button type="submit"  label="Accept" size={"large"}/>
+                <Button type="reset" label="Clear" size={"large"}/>
+            </Box>
+        </Box>
+    );
+}
---
+++ src/components/XDiagram.js
@@ -0,0 +1,5 @@
+import {Box, Grommet} from "grommet";
+
+export default  () => {
+
+}
