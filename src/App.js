dbe98c9a4e3258a53ed931dc6589c0004fb7307e
Milord <immunitygame@gmail.com>
2021-08-25 07:15:55 -0400

    added prebuild step and removed commented out code

--- src/App.js
+++ src/App.js
@@ -134,34 +134,4 @@ function App() {
 }
 
 
-
-
-
-// class App extends Component {
-//     render() {
-//       return (
-//         <CacheBuster>
-//           {({ loading, isLatestVersion, refreshCacheAndReload }) => {
-//             if (loading) return null;
-//             if (!loading && !isLatestVersion) {
-//               refreshCacheAndReload();
-//             }
-  
-//             return (
-//               <div className="App">
-//                 <header className="App-header">
-//                   <h1>Cache Busting - Example</h1>
-//                   <p>
-//                     Bundle version - <code>v{global.appVersion}</code>
-//                   </p>
-//                 </header>
-//               </div>
-//             );
-//           }}
-//         </CacheBuster>
-//       );
-//     }
-//   }
-
-
 export default App;
